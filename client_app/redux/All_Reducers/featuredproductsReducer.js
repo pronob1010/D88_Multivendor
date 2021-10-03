a373eb60662f7af4dc6da28ae0cf5c9ccb9036d3
tania18115934 <tania15-934@diu.edu.bd>
2021-10-03 15:57:24 +0600

    slider done

--- client_app/redux/All_Reducers/featuredproductsReducer.js
+++
@@ -1,9 +0,0 @@
-import FeaturedProductsList from './../../Data/featured_products';
-
-const initialState = {
-    products : FeaturedProductsList,
-}
-
-export const FeaturedProductReducer = (state = initialState, action) => {
-    return state;
-}
---
+++ client_app/redux/All_Reducers/promotionalproductsReducer.js
@@ -0,0 +1,9 @@
+import PromotionalProductsList from './../../Data/promotional_products';
+
+const initialState = {
+    products : PromotionalProductsList,
+}
+
+export const PromotionalProductReducer = (state = initialState, action) => {
+    return state;
+}
