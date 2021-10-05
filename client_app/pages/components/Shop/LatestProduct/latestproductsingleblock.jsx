314411601aa177ed82fd2aef15ae35f0800cc9d3
tania18115934 <tania15-934@diu.edu.bd>
2021-10-05 17:59:11 +0600

    letest product navigation added

--- client_app/pages/components/Shop/LatestProduct/latestproductsingleblock.jsx
+++ client_app/pages/components/Shop/LatestProduct/latestproductsingleblock.jsx
@@ -3,12 +3,12 @@ export default function LatestProductSingleblock({item}){
         <li className="mb-4">
         <div className="row">
             <div className="col-auto">
-                <a href="../shop/single-product-fullwidth.html" className="d-block width-75">
+                <a href={`/shop/${item.id}`} className="d-block width-75">
                     <img className="img-fluid" src={item.image} alt="Image Description" />
                 </a>
             </div>
             <div className="col">
-                <h3 className="text-lh-1dot2 font-size-14 mb-0"><a href="../shop/single-product-fullwidth.html">{item.Title}</a></h3>
+                <h3 className="text-lh-1dot2 font-size-14 mb-0"><a href={`/shop/${item.id}`}>{item.Title}</a></h3>
                 {/* <div className="text-warning text-ls-n2 font-size-16 mb-1" style={{ width: "80px" }}>
                     <small className="fas fa-star"></small>
                     <small className="fas fa-star"></small>
