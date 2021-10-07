abc782965944962c12993e7d8061f8aea6be973e
pronob1010 <pronobmozumder.info@gmail.com>
2021-10-07 08:05:33 +0600

    product final increment/decriment

--- client_app/pages/components/detailCart/singleproductblock.jsx
+++ client_app/pages/components/detailCart/singleproductblock.jsx
@@ -1,7 +1,9 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from 'react';
 
 
 export default function SingleProductBlock({ item }) {
+
 
     let uid = item.product_id;
 
@@ -12,6 +14,43 @@ export default function SingleProductBlock({ item }) {
     let mod_title = String(singleProductData.Title).substring(0, 35) + "...";
 
     let this_product_final_price = singleProductData.offer_price * item.product_quantity;
+
+    const [final_quantity, setfinal_quantity] = useState(item.product_quantity);
+
+  const dispatch = useDispatch();
+
+    // function cartHandeler(d_quantity) {
+        
+    //     dispatch({
+    //       type : "ADD_TO_CART_FINAL",
+    //       value : uid,
+    //       quantity : d_quantity,
+    //   });
+    // }
+
+
+    function minusHandel() {
+        if (final_quantity > 1) {
+            setfinal_quantity(final_quantity - 1);
+
+            dispatch({
+                type : "ADD_TO_CART_FINAL",
+                value : uid,
+                quantity : final_quantity-1,
+            });
+        }
+      }
+    
+      function plusHandel() {
+        setfinal_quantity(final_quantity + 1);
+        dispatch({
+            type : "ADD_TO_CART_FINAL",
+            value : uid,
+            quantity : final_quantity+1,
+        });
+      }
+
+    
 
     return (
         <tr>
@@ -36,13 +75,13 @@ export default function SingleProductBlock({ item }) {
                 <div className="border rounded-pill py-1 width-122 w-xl-80 px-3 border-color-1">
                     <div className="js-quantity row align-items-center">
                         <div className="col">
-                            <input className="js-result form-control h-auto border-0 rounded p-0 shadow-none" type="text" value={item.product_quantity} />
+                            <input className="js-result form-control h-auto border-0 rounded p-0 shadow-none" type="text" value={final_quantity} />
                         </div>
                         <div className="col-auto pr-1">
-                            <a className="js-minus btn btn-icon btn-xs btn-outline-secondary rounded-circle border-0" href="javascript:;">
+                            <a className="js-minus btn btn-icon btn-xs btn-outline-secondary rounded-circle border-0" href="javascript:;" onClick={minusHandel}>
                                 <small className="fas fa-minus btn-icon__inner"></small>
                             </a>
-                            <a className="js-plus btn btn-icon btn-xs btn-outline-secondary rounded-circle border-0" href="javascript:;">
+                            <a className="js-plus btn btn-icon btn-xs btn-outline-secondary rounded-circle border-0" href="javascript:;" onClick={plusHandel}>
                                 <small className="fas fa-plus btn-icon__inner"></small>
                             </a>
                         </div>
