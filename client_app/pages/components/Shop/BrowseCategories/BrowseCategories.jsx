8f40b0372067ec79c86d36a785eb6d9c554f607d
Tania Islam <tania15-1863@diu.edu.bd>
2021-12-07 13:51:49 +0600

    all page cat. and sub. cat. added

--- client_app/pages/components/Shop/BrowseCategories/BrowseCategories.jsx
+++ client_app/pages/components/Shop/BrowseCategories/BrowseCategories.jsx
@@ -2,161 +2,48 @@ import IndexBrowseCategoriesMainSingle from './IndexBrowseCategoriesMainSingle';
 import { useSelector } from 'react-redux';
 
 export default function BrowseCategories() {
+
+    let categories_list = useSelector(state => state.categoryState.categories);
+    
+
+    let cat_list = categories_list.map(item => {
+
+        let slug_id = item.slug;
+        let c_id = slug_id + 'Collapse';
+        let h_id = '#'+ c_id;
+        
+        return (
+            <li>
+                <a className="dropdown-toggle dropdown-toggle-collapse" href="javascript:;" role="button" data-toggle="collapse" aria-expanded="false" aria-controls={c_id} data-target={h_id}>
+                    {item.title}<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span>
+                </a>
+
+                <div id={c_id} className="collapse" data-parent="#sidebarNav">
+                    <ul id={slug_id} className="list-unstyled dropdown-list">
+                        {
+                            item.subcategory.map(item => {
+                                return (
+                                    <li><a className="dropdown-item" href="#">{item.title}<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
+                                )
+                            })
+                        }
+                        {/* <!--Menu List --> */}
+                        {/* <!--End Menu List --> */}
+                    </ul>
+                </div>
+            </li>
+        )
+    })
+
     return (
         <div className="mb-6 border border-width-2 border-color-3 borders-radius-6">
             {/* <!--List --> */}
             <ul id="sidebarNav" className="list-unstyled mb-0 sidebar-navbar view-all">
                 <li><div className="dropdown-title">Browse Categories</div></li>
-                <li>
-                    <a className="dropdown-toggle dropdown-toggle-collapse" href="javascript:;" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="sidebarNav1Collapse" data-target="#sidebarNav1Collapse">
-                        Accessories<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span>
-                    </a>
-
-                    <div id="sidebarNav1Collapse" className="collapse" data-parent="#sidebarNav">
-                        <ul id="sidebarNav1" className="list-unstyled dropdown-list">
-                            {/* <!--Menu List --> */}
-                            <li><a className="dropdown-item" href="#">Accessories<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Cameras & Photography<span className="text-gray-25 font-size-12 font-weight-normal"> (11)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Computer Components<span className="text-gray-25 font-size-12 font-weight-normal"> (22)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Gadgets<span className="text-gray-25 font-size-12 font-weight-normal"> (5)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Home Entertainment<span className="text-gray-25 font-size-12 font-weight-normal"> (7)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Laptops & Computers<span className="text-gray-25 font-size-12 font-weight-normal"> (42)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Printers & Ink<span className="text-gray-25 font-size-12 font-weight-normal"> (63)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Smart Phones & Tablets<span className="text-gray-25 font-size-12 font-weight-normal"> (11)</span></a></li>
-                            <li><a className="dropdown-item" href="#">TV & Audio<span className="text-gray-25 font-size-12 font-weight-normal"> (66)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Video Games & Consoles<span className="text-gray-25 font-size-12 font-weight-normal"> (31)</span></a></li>
-                            {/* <!--End Menu List --> */}
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a className="dropdown-toggle dropdown-toggle-collapse" href="javascript:;" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="sidebarNav2Collapse" data-target="#sidebarNav2Collapse">
-                        Cameras & Photography<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span>
-                    </a>
-
-                    <div id="sidebarNav2Collapse" className="collapse" data-parent="#sidebarNav">
-                        <ul id="sidebarNav2" className="list-unstyled dropdown-list">
-                            {/* <!--Menu List --> */}
-                            <li><a className="dropdown-item" href="#">Cameras<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            {/* <!--End Menu List --> */}
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a className="dropdown-toggle dropdown-toggle-collapse" href="javascript:;" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="sidebarNav3Collapse" data-target="#sidebarNav3Collapse">
-                        Computer Components<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span>
-                    </a>
-
-                    <div id="sidebarNav3Collapse" className="collapse" data-parent="#sidebarNav">
-                        <ul id="sidebarNav3" className="list-unstyled dropdown-list">
-                            {/* <!--Menu List --> */}
-                            <li><a className="dropdown-item" href="#">Computer Cases<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            {/* <!--End Menu List --> */}
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a className="dropdown-toggle dropdown-toggle-collapse" href="javascript:;" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="sidebarNav4Collapse" data-target="#sidebarNav4Collapse">
-                        Gadgets<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span>
-                    </a>
-
-                    <div id="sidebarNav4Collapse" className="collapse" data-parent="#sidebarNav">
-                        <ul id="sidebarNav4" className="list-unstyled dropdown-list">
-                            {/* <!--Menu List --> */}
-                            <li><a className="dropdown-item" href="#">Smartwatches<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Wearables<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            {/* <!--End Menu List --> */}
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a className="dropdown-toggle dropdown-toggle-collapse" href="javascript:;" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="sidebarNav5Collapse" data-target="#sidebarNav5Collapse">
-                        Home Entertainment<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span>
-                    </a>
-
-                    <div id="sidebarNav5Collapse" className="collapse" data-parent="#sidebarNav">
-                        <ul id="sidebarNav5" className="list-unstyled dropdown-list">
-                            {/* <!--Menu List --> */}
-                            <li><a className="dropdown-item" href="#">Tvs<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            {/* <!--End Menu List --> */}
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a className="dropdown-toggle dropdown-toggle-collapse" href="javascript:;" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="sidebarNav6Collapse" data-target="#sidebarNav6Collapse">
-                        Laptops & Computers<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span>
-                    </a>
-
-                    <div id="sidebarNav6Collapse" className="collapse" data-parent="#sidebarNav">
-                        <ul id="sidebarNav6" className="list-unstyled dropdown-list">
-                            {/* <!--Menu List --> */}
-                            <li><a className="dropdown-item" href="#">Accessories<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Cameras & Photography<span className="text-gray-25 font-size-12 font-weight-normal"> (11)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Computer Components<span className="text-gray-25 font-size-12 font-weight-normal"> (22)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Gadgets<span className="text-gray-25 font-size-12 font-weight-normal"> (5)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Home Entertainment<span className="text-gray-25 font-size-12 font-weight-normal"> (7)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Laptops & Computers<span className="text-gray-25 font-size-12 font-weight-normal"> (42)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Printers & Ink<span className="text-gray-25 font-size-12 font-weight-normal"> (63)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Smart Phones & Tablets<span className="text-gray-25 font-size-12 font-weight-normal"> (11)</span></a></li>
-                            <li><a className="dropdown-item" href="#">TV & Audio<span className="text-gray-25 font-size-12 font-weight-normal"> (66)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Video Games & Consoles<span className="text-gray-25 font-size-12 font-weight-normal"> (31)</span></a></li>
-                            {/* <!--End Menu List --> */}
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a className="dropdown-toggle dropdown-toggle-collapse" href="javascript:;" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="sidebarNav7Collapse" data-target="#sidebarNav7Collapse">
-                        Printers & Ink<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span>
-                    </a>
-
-                    <div id="sidebarNav7Collapse" className="collapse" data-parent="#sidebarNav">
-                        <ul id="sidebarNav7" className="list-unstyled dropdown-list">
-                            {/* <!--Menu List --> */}
-                            <li><a className="dropdown-item" href="#">Printers<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            {/* <!--End Menu List --> */}
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a className="dropdown-toggle dropdown-toggle-collapse" href="javascript:;" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="sidebarNav8Collapse" data-target="#sidebarNav8Collapse">
-                        Smart Phones & Tablets<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span>
-                    </a>
-
-                    <div id="sidebarNav8Collapse" className="collapse" data-parent="#sidebarNav">
-                        <ul id="sidebarNav8" className="list-unstyled dropdown-list">
-                            {/* <!--Menu List --> */}
-                            <li><a className="dropdown-item" href="#">Smartphones<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            <li><a className="dropdown-item" href="#">Tablets<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            {/* <!--End Menu List --> */}
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a className="dropdown-toggle dropdown-toggle-collapse" href="javascript:;" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="sidebarNav9Collapse" data-target="#sidebarNav9Collapse">
-                        TV & Audio<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span>
-                    </a>
-
-                    <div id="sidebarNav9Collapse" className="collapse" data-parent="#sidebarNav">
-                        <ul id="sidebarNav9" className="list-unstyled dropdown-list">
-                            {/* <!--Menu List --> */}
-                            <li><a className="dropdown-item" href="#">Audio Speakers<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            {/* <!--End Menu List --> */}
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a className="dropdown-toggle dropdown-toggle-collapse" href="javascript:;" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="sidebarNav10Collapse" data-target="#sidebarNav10Collapse">
-                        Video Games & Consoles<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span>
-                    </a>
-
-                    <div id="sidebarNav10Collapse" className="collapse" data-parent="#sidebarNav">
-                        <ul id="sidebarNav10" className="list-unstyled dropdown-list">
-                            {/* <!--Menu List --> */}
-                            <li><a className="dropdown-item" href="#">Game Consoles<span className="text-gray-25 font-size-12 font-weight-normal"> (56)</span></a></li>
-                            {/* <!--End Menu List --> */}
-                        </ul>
-                    </div>
-                </li>
+                 
+                
+                 {cat_list}
+                 
             </ul>
             {/* <!--End List --> */}
         </div>
